16d4980ac4728717b6a7b836fafce4823496ec6c
gabriellesilva018 <gabriellejuliaa319@gmail.com>
2026-08-28 01:18:58 -0300

    Arrumando estetica

--- Main.java
+++ Main.java
@@ -6,22 +6,22 @@ public class Main {
         System.out.println("=== PETSHOP DEVOPS ===");
 
         //CRIA O PET
-        Pet pet1 = new Pet("Rex", "Cachorro", 5, 150.00);
-        Pet pet2 = new Pet("Pandora", "Gata", 2, 100.00);
+        Pet Pet1 = new Pet("Rex", "Cachorro", 5, 150.00);
+        Pet Pet2 = new Pet("Pandora", "Gata", 2, 100.00);
         Pet pet3 = new Pet("Tempestade", "Cavalo",7, 300.00);
 
-        System.out.println("\n pet1");
-        pet1.exibirInformacoes();
-        System.out.println("Valor do Serviço R$: " + pet1.calcularComDesconto());
+        System.out.println("\n Pet1");
+        Pet1.exibirInformacoes();
+        System.out.println("Valor do Serviço R$: " + Pet1.calcularComDesconto());
 
 
-        System.out.println("\n pet2");
-        pet2.exibirInformacoes();
-        System.out.println("Valor do Serviço R$: " + pet2.calcularComDesconto());
+        System.out.println("\n Pet2");
+        Pet2.exibirInformacoes();
+        System.out.println("Valor do Serviço R$: " + Pet2.calcularComDesconto());
 
-    System.out.println("\n pet3");
-    pet3.exibirInformacoes();
-    System.out.println("Valor do Serviço R$: " + pet3.calcularComDesconto());
+    System.out.println("\n Pet3");
+    Pet3.exibirInformacoes();
+    System.out.println("Valor do Serviço R$: " + Pet3.calcularComDesconto());
     System.out.println("Aplicação executada com sucesso");    
 
     }
